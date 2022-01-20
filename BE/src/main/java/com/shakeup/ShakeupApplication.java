83f2a00562824cbed31e0ea29cd8db2c2822731b
ekdms4213 <38871826+ekdms4213@users.noreply.github.com>
2022-01-20 11:11:02 +0900

    create feature/BE/start

---
+++ BE/src/main/java/com/shakeup/ShakeupApplication.java
@@ -0,0 +1,13 @@
+package com.shakeup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShakeupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShakeupApplication.class, args);
+	}
+
+}
