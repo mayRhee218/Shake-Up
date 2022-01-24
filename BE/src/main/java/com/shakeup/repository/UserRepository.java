93016e2c2420d03cf3cbbc5dd3378fdbe7b5c4b9
ekdms4213 <38871826+ekdms4213@users.noreply.github.com>
2022-01-24 11:20:32 +0900

    update feature/BE/signup

--- BE/src/main/java/com/shakeup/repository/UserRepository.java
+++ BE/src/main/java/com/shakeup/repository/UserRepository.java
@@ -10,5 +10,5 @@ public interface UserRepository extends JpaRepository<Users, Long> {
     Optional<Users> findByEmail(String email);   // 검색 쿼리를 실행한 결과를 전달
     Optional<Users> findById(String id);
     Optional<Users> findByUid(Long uid);
-    Optional<Users> findByName(String name);
+    Optional<Users> findByName(String name);    // 채널명 검색
 }
