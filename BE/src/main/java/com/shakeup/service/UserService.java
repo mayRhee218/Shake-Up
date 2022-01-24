79e57266d5f6feed8e59353eaf13ae2634988c92
ekdms4213 <38871826+ekdms4213@users.noreply.github.com>
2022-01-24 11:08:34 +0900

    update feature/BE/signup

--- BE/src/main/java/com/shakeup/service/UserService.java
+++ BE/src/main/java/com/shakeup/service/UserService.java
@@ -2,6 +2,7 @@ package com.shakeup.service;
 
 import com.shakeup.model.Users;
 import com.shakeup.repository.UserRepository;
+import com.shakeup.request.UserSignUpRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +20,32 @@ public class UserService {
             return user;
         } else {
             return null;
+        }
+    }
+
+    public String signUp(UserSignUpRequest userSignUpRequest){
+        Users tempuser = userSignUpRequest.toEntity();
+        userRepository.save(tempuser);
+        return "success";
+    }
+
+    public String checkId(String id){
+        Optional<Users> user = userRepository.findById(id);
+        System.out.println(user);
+        if (user.isPresent()) {
+            return "fail";
+        } else {
+            return "success";
+        }
+    }
+
+    public String checkName(String name){
+        Optional<Users> user = userRepository.findByName(name);
+        System.out.println(user);
+        if (user.isPresent()) {
+            return "fail";
+        } else {
+            return "success";
         }
     }
 }
