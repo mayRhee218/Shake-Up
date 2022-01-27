a3e70cc5596281550c1b91f658bec61a4ea94512
unknown <dlarlxo23@naver.com>
2022-01-27 10:45:46 +0900

    create feature/BE/video

---
+++ BE/src/main/java/com/shakeup/service/VideoService.java
@@ -0,0 +1,61 @@
+package com.shakeup.service;
+
+import com.shakeup.model.Videos;
+import com.shakeup.repository.VideoRepository;
+import com.shakeup.request.video.VideoCreateRequest;
+import com.shakeup.request.video.VideoUpdateRequest;
+import com.sun.istack.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class VideoService {
+
+    @Autowired
+    private VideoRepository videoRepository;
+
+    //영상 DB저장
+    public String createVideo(VideoCreateRequest videoCreateRequest) {
+        Videos tempvideo = videoCreateRequest.toEntity();
+        try {
+            videoRepository.save(tempvideo);
+            return "success";
+        } catch (Exception e) {
+            return "fail";
+        }
+
+    }
+
+    public String upadateVideo(VideoUpdateRequest videoUpdateRequest) {
+        Videos tempvideo = videoUpdateRequest.toEntity();
+        Optional<Videos> video = videoRepository.findVideosByVid(tempvideo.getVid());
+
+        if (video.isPresent()) {
+            video.get().setTitle(tempvideo.getTitle());
+            video.get().setUrl(tempvideo.getUrl());
+            video.get().setCategory(tempvideo.getCategory());
+            video.get().setShow(tempvideo.getShow());
+            video.get().setThumbnail(tempvideo.getThumbnail());
+
+            videoRepository.save(video.get());
+
+            return "success";
+        }
+        return "fail";
+    }
+    public String deleteVideos(long vid){
+        Optional<Videos> video = videoRepository.findVideosByVid(vid);
+
+        if(video.isPresent()){
+            videoRepository.deleteById(video.get().getVid());
+            return "success";
+        }
+        return "fail";
+    }
+
+
+    //Read를 하는데..... 어떡하지 그냥 카테고리만 입력 받고 하면 될려나?
+
+}
