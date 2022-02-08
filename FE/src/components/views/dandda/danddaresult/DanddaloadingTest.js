292f3bb4fc13fa9d31d0a798d6a6710d28452a6c
ekdms4213 <38871826+ekdms4213@users.noreply.github.com>
2022-02-09 02:39:29 +0900

    update feature/FE/reactTensorflow #305

--- FE/src/components/views/dandda/danddaresult/DanddaloadingTest.js
+++ FE/src/components/views/dandda/danddaresult/DanddaloadingTest.js
@@ -5,22 +5,52 @@
 import { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 
+const express = require("express");
 const TeachableMachine = require("@sashido/teachablemachine-node");
+const bodyParser = require("body-parser");
 
 const model = new TeachableMachine({
   modelUrl: "https://teachablemachine.withgoogle.com/models/0BEUR8k_M/",
 });
 
-model
-  .classify({
-    imageUrl: "https://www.techinn.com/f/13777/137777410/apple-ipad-32gb-10.2-tablet.jpg",
-  })
-  .then((predictions) => {
-    console.log("Predictions:", predictions);
-  })
-  .catch((e) => {
-    console.log("ERROR", e);
-  });
+const app = express();
+app.use(express.json());
+app.use(bodyParser.json());
+const port = process.env.port || 5000;
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send(`
+    <form action="/image/classify" method="POST">
+    <p>
+    Enter Image Url</p>
+    <input name='ImageUrl' autocomplete=off>
+    <button>Predict Image</button>
+    </form>
+    `);
+});
+
+app.post("/image/classify", async (req, res) => {
+  const url = req.body.ImageUrl;
+
+  return model
+    .classify({
+      imageUrl: url,
+    })
+    .then((predictions) => {
+      console.log(predictions);
+      res.json(predictions);
+    })
+    .catch((e) => {
+      console.error(e);
+      res.status(500).send("Something went wrong!");
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
 
 const TmPose = () => {};
 
