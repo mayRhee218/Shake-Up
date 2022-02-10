8101fbaeead479b7105686f20073b2a3fa763b59
ekdms4213 <38871826+ekdms4213@users.noreply.github.com>
2022-02-11 04:36:50 +0900

    update feature/FE/reactTensorflow #305

--- FE/src/components/views/dandda/danddaresult/DanddaloadingTest.js
+++ FE/src/components/views/dandda/danddaresult/DanddaloadingTest.js
@@ -1,6 +1,11 @@
 /**
- * @description Make an image classification app using React and Tensorflow JS
- */
+ * 모델 인식 결과 페이지
+ *
+ * @author 다은
+ * @version 1.0.0
+ * 작성일 : 2022-02-11
+ *
+ **/
 
 import { useEffect, useState, useRef } from "react";
 import React from "react";
@@ -11,37 +16,41 @@ import { getFile } from "../../firebase/db";
 
 function TmPose() {
   const [isModelLoading, setIsModelLoading] = useState(false);
-  // const [model, setModel] = useState(null);
+  const [model, setModel] = useState(null);
   const [videoURL, setVideoURL] = useState(null);
   const [results, setResults] = useState([]);
   const [labels, setLabels] = useState(null);
+  const [maxPredictions, setMaxPredictions] = useState(null);
+  const [animationFrame, setAnimationFrame] = useState(null);
+  const [correctCount, setCorrectCount] = useState(0);
 
   const videoRef = useRef();
-
-  let model, maxPredictions;
 
   // 모델 로딩 함수
   const loadModel = async () => {
     // Next Level 학습 모델
-    const URL = "https://teachablemachine.withgoogle.com/models/Fwie9ZFvv/";
+    const URL = "https://teachablemachine.withgoogle.com/models/8a2i874rC/";
     const modelURL = URL + "model.json";
     const metadataURL = URL + "metadata.json";
 
     setIsModelLoading(true);
     try {
-      model = await tmPose.load(modelURL, metadataURL);
-      maxPredictions = model.getTotalClasses();
+      const model = await tmPose.load(modelURL, metadataURL);
 
-      // setModel(model);
-      console.log("모델 로딩 성공");
+      const maxPredictions = model.getTotalClasses();
+
+      setModel(model);
+      setMaxPredictions(maxPredictions);
       setIsModelLoading(false);
+
+      console.log("모델 로딩 성공");
 
       // 클래스 개수만큼 div 추가
       let label = "";
-      console.log("maxPredictions : " + maxPredictions);
       for (let i = 0; i < maxPredictions; i++) {
-        label += "<div>엘리먼트 수정</div>";
+        label += "<div>클래스 이름 : 정확도</div>";
       }
+
       setLabels(label);
     } catch (error) {
       console.log(error);
@@ -51,22 +60,76 @@ function TmPose() {
 
   // 파이어 베이스의 video url 가져오기
   const uploadFirebaseVideo = async () => {
-    const res = await getFile("12345.mp4");
+    const res = await getFile("daeun_test.mp4");
     setVideoURL(res);
   };
 
   // 인식하기
-  const identify = async () => {
-    console.log("identify 함수 호출");
+  let cnt = 0; // 맞춘 개수
+  let startTimeSeconds; // 시작 시간 가져오기
+  let curTimeSeconds; // 현재 시간 가져오기
+  let timeSeconds; // (현재 시간 - 시작 시간) => 경과한 시간(초) 구하기
+  let checkCount0 = false,
+    checkCount1 = false,
+    checkCount2 = false; // 동작이 맞았는지 체크
 
-    const video = document.querySelector(".");
-    console.log("identify 함수 호출2");
-    const { pose, posenetOutput } = await model.estimatePose(videoRef, false);
-    console.log("identify 함수 호출3");
+  const identify = async () => {
+    const labelContainer = document.querySelector(".label-container");
+    const { pose, posenetOutput } = await model.estimatePose(videoRef.current, false);
     const results = await model.predict(posenetOutput);
-    console.log("identify 함수 호출4");
+
     setResults(results);
-    console.log("result : " + results);
+
+    // 경과 시간 구하기
+    curTimeSeconds = new Date().getSeconds();
+    timeSeconds = curTimeSeconds - startTimeSeconds;
+
+    // 인식
+    for (let i = 0; i < maxPredictions; i++) {
+      // 클래스 이름 : 정확도 innerHTML로 넣기
+      const prediction = results[i].className + ": " + results[i].probability.toFixed(2);
+      labelContainer.childNodes[i].innerHTML = prediction;
+
+      // left
+      if (results[0].probability.toFixed(2) > 0.9) {
+        if (timeSeconds >= 0 && timeSeconds <= 2) {
+          if (!checkCount0) {
+            console.log("left 인식 => 경과 시간 : " + timeSeconds);
+            checkCount0 = true; // 반복문 안에서 setState 쓰면 리렌더링이 안되므로 쓰면 X
+            setCorrectCount(++cnt);
+          }
+        }
+      }
+      // right
+      else if (results[1].probability.toFixed(2) > 0.9) {
+        if (timeSeconds >= 3 && timeSeconds <= 4) {
+          if (!checkCount1) {
+            console.log("right 인식 => 경과 시간 : " + timeSeconds);
+            checkCount1 = true;
+            setCorrectCount(++cnt);
+          }
+        }
+      }
+      // default
+      // else if (results[2].probability.toFixed(2) > 0.9) {
+      //   if (!checkCount2) {
+      //     checkCount2 = true;
+      //     setCorrectCount(++cnt);
+      //   }
+      // }
+    }
+  };
+
+  // 동영상 인식 반복 호출
+  const loop = async () => {
+    await identify();
+    setAnimationFrame(window.requestAnimationFrame(loop));
+  };
+
+  // loop 함수 호출하기 (이 함수는 한번만 실행, startTime을 구하기 위해 사용)
+  const startLoop = () => {
+    startTimeSeconds = new Date().getSeconds();
+    loop();
   };
 
   // 모델 로딩
@@ -80,9 +143,16 @@ function TmPose() {
     return <h2>Model Loading...</h2>;
   }
 
+  // 비디오가 끝나면 인식 멈춤
+  const myCallback = () => {
+    return window.cancelAnimationFrame(animationFrame);
+  };
+
   return (
-    <div className="TmPose">
-      <h1 className="header">Video Identification</h1>
+    <div className="TmPose" style={{ textAlign: "center" }}>
+      <h1 className="header" style={{ textAlign: "center" }}>
+        Video Identification
+      </h1>
       <div className="mainWrapper">
         <div className="mainContent">
           <div className="videoHolder">
@@ -93,41 +163,36 @@ function TmPose() {
                 src={videoURL}
                 width="300"
                 height="300"
+                crossOrigin="anonymous" // 이거 없으면 model.estimatePose 실행 안됨★
                 ref={videoRef}
                 autoPlay
+                // controls
                 muted
+                onPlay={startLoop}
+                onEnded={() => myCallback()} // 비디오 끝나면 인식 멈춤
               ></video>
             )}
           </div>
-
-          <div className="result-container">
-            {/* {results.length > 0 && (
-            <div className="resultsHolder">
-              {results.map((result, index) => {
-                return (
-                  <div className="result" key={result.className}>
-                    <span className="name">{result.className}</span>
-                    <span className="confidence">
-                      Confidence level: {(result.probability * 100).toFixed(2)}%{" "}
-                      {index === 0 && <span className="bestGuess">Best Guess</span>}
-                    </span>
-                  </div>
-                );
-              })}
-            </div>
-          )} */}
-          </div>
         </div>
 
-        <div className="identify-container">
+        {/* <div className="identify-container">
           {videoURL && (
-            <button className="button" onClick={identify}>
+            <button className="button" onClick={startLoop}>
               인식 버튼을 눌러주세용
             </button>
           )}
         </div>
+        <br></br> */}
 
-        <div dangerouslySetInnerHTML={{ __html: labels }} className="label-container"></div>
+        <div className="label-container" dangerouslySetInnerHTML={{ __html: labels }}></div>
+        <br></br>
+        <div className="result-container">
+          맞춘 동작 개수
+          {/* 몇 개 맞췄는지 결과 내기 */}
+          <div className="resultContent">
+            {correctCount} / {maxPredictions}
+          </div>
+        </div>
       </div>
     </div>
   );
