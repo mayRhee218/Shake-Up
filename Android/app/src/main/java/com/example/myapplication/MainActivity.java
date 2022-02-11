ad65757c28089c55c168a3571a5119f3d34fe460
최성석 <tjdtjr234@naver.com>
2022-02-11 10:32:39 +0900

    merge feature/FE/loading #300

--- Android/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Android/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,5 +1,10 @@
 package com.example.myapplication;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -27,17 +32,21 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity {
 
     private  WebView mwv;//Mobile Web View
     public static Context mContext;
-    public String videoUrl ="";
+    public String videoUrl;
     private static final int RC_FILE_CHOOSE = 2833;
     private ValueCallback<Uri> mUploadMsg = null;
-
+    private ActivityResultLauncher<Intent> resultLauncher;
+    public String test = "테스트용";
 
     static final int REQUEST_VIDEO_CAPTURE = 1;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
 //        setContentView(R.layout.camera_test);
         mContext = this.getApplicationContext();
 
+
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();   //타이틀바 숨기기
 
@@ -53,40 +63,31 @@ public class MainActivity extends AppCompatActivity {
         mwv.addJavascriptInterface(new WebAppInterface(this), "Android");
         WebSettings mws=mwv.getSettings();//Mobile Web Setting
         mws.setDomStorageEnabled(true);
-        mws.setJavaScriptEnabled(true);//자바스크립트 허용
         mws.setLoadWithOverviewMode(true);//컨텐츠가 웹뷰보다 클 경우 스크린 크기에 맞게 조정
-        mwv.loadUrl("http://121.159.4.37:3000/");
-        mwv.setWebViewClient(new WebViewClientClass());
-    }
-    private class WebViewClientClass extends WebViewClient {
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            if (url.startsWith("app://")) {
-                Intent intent = new Intent(mContext.getApplicationContext(), CameraActivity.class);
-//                startActivity(intent);
-                startActivityForResult(intent,0);
-                return true;
-            }
-            else {
-                view.loadUrl(url);
-                return true;
-            }
-        }
-    }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        super.onActivityResult(requestCode, resultCode, data);
+        // Javascript 사용하기
+        mws.setJavaScriptEnabled(true);
+        // WebView 내장 줌 사용여부
+        mws.setBuiltInZoomControls(true);
+        // 화면에 맞게 WebView 사이즈를 정의
+        mws.setLoadWithOverviewMode(true);
+        // ViewPort meta tag를 활성화 여부
+        mws.setUseWideViewPort(true);
+        // 줌 컨트롤 사용 여부
+        mws.setDisplayZoomControls(false);
+        // 사용자 제스처를 통한 줌 기능 활성화 여부
+        mws.setSupportZoom(false);
+        // TextEncoding 이름 정의
+        mws.setDefaultTextEncodingName("UTF-8");
 
-        if(requestCode==0){
-            if (resultCode==RESULT_OK) {
-                Toast.makeText(mContext, "로딩 페이지 작동 성공" , Toast.LENGTH_LONG).show();
-                mwv.loadUrl("javascript.loadingPage()");
-            }else{
-                Toast.makeText(mContext, "로딩 페이지 작동 실패" , Toast.LENGTH_LONG).show();
-            }
-        }else if(requestCode==1){
-        }
+        // Setting Local Storage
+        mws.setDatabaseEnabled(true);
+        mws.setDomStorageEnabled(true);
+
+        // 캐쉬 사용 방법을 정의
+        mws.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        mwv.loadUrl("http://121.159.4.37:3000/");
     }
+
 
     public class WebAppInterface {
         Context mContext;
@@ -96,19 +97,55 @@ public class MainActivity extends AppCompatActivity {
             mContext = c;
         }
 
-
-
         /** Show a toast from the web page */
         @JavascriptInterface
         public void showToast(String toast, String url) {
             //토스트 메시지 보여주기
-            Toast.makeText(mContext, toast , Toast.LENGTH_LONG).show();
+//            Toast.makeText(mContext, toast , Toast.LENGTH_LONG).show();
             //카메라 실행
-            Intent intent = new Intent(MainActivity.this, CameraActivity.class);
+            Intent intent = new Intent(MainActivity.this, CAMERA2_Activity.class);
             intent.putExtra("동영상링크",url);
-            startActivity(intent);
+            startActivityResult.launch(intent);
         }
+
+        ActivityResultLauncher<Intent> startActivityResult = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == RESULT_OK) {
+//                            Toast.makeText(mContext, "로딩 페이지 작동 성공", Toast.LENGTH_LONG).show();
+                            mwv.loadUrl("http://121.159.4.37:3000/loading");
+                            //http://121.159.4.37:3000/
+                            System.out.println("이전에 실행됨 ");
+                            
+                        } else {
+//                            Toast.makeText(mContext, "로딩 페이지 작동 실패", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }
+        );
+
+
     }
+
+
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if (requestCode == 0) {
+//            if (resultCode == RESULT_OK) {
+//                Toast.makeText(mContext, "로딩 페이지 작동 성공", Toast.LENGTH_LONG).show();
+//                mwv.loadUrl("javascript:loadingPage()");
+//            } else {
+//                Toast.makeText(mContext, "로딩 페이지 작동 실패", Toast.LENGTH_LONG).show();
+//            }
+//        } else {
+//            Toast.makeText(mContext, "requestCode가 0이 아님 [ " + requestCode + " ]", Toast.LENGTH_LONG).show();
+//        }
+//    }
 
 
 
