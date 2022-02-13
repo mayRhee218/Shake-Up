faf0f71a8ca896ffc7cc5e622bbb2e67b7673c25
ekdms4213 <38871826+ekdms4213@users.noreply.github.com>
2022-02-14 04:58:16 +0900

    update Danddaloading.js Dance basics Bounce

--- FE/src/components/views/dandda/danddaresult/Danddaloading.js
+++ FE/src/components/views/dandda/danddaresult/Danddaloading.js
@@ -44,9 +44,18 @@ function Danddaloading() {
   let cnt = 0; // 맞춘 개수
   let startTimeSeconds; // 시작 시간 가져오기
   let curTimeSeconds; // 현재 시간 가져오기
-  let timeSeconds; // (현재 시간 - 시작 시간) => 경과한 시간(초) 구하기
-  let checkCount0 = false,
-    checkCount1 = false; // 동작이 맞았는지 체크
+  let t; // (현재 시간 - 시작 시간) => 경과한 시간(초) 구하기
+  // 동작이 맞았는지 체크
+  let m0c0 = false,
+    m0c1 = false,
+    m0c2 = false,
+    m0c3 = false,
+    m0c4 = false,
+    m0c5 = false,
+    m0c6 = false,
+    m0c7 = false,
+    m0c8 = false,
+    m0c9 = false;
   let URL;
 
   // 모델 URL 값이 세팅 되었을 때, 모델 로딩 함수 실행
@@ -122,31 +131,131 @@ function Danddaloading() {
 
     // 경과 시간 구하기
     curTimeSeconds = new Date().getSeconds();
-    timeSeconds = curTimeSeconds - startTimeSeconds;
+    t = curTimeSeconds - startTimeSeconds;
 
-    // 인식
-    for (let i = 0; i < maxPredictions; i++) {
-      // 클래스 이름 : 정확도 innerHTML로 넣기
-      const prediction = results[i].className + ": " + results[i].probability.toFixed(2);
-      labelContainer.childNodes[i].innerHTML = prediction;
+    // 바운스
+    if (turl === "https://teachablemachine.withgoogle.com/models/NuAS299xH/") {
+      for (let i = 0; i < maxPredictions; i++) {
+        // 클래스 이름 : 정확도 innerHTML로 넣기
+        const prediction = results[i].className + ": " + results[i].probability.toFixed(2);
+        labelContainer.childNodes[i].innerHTML = prediction;
 
-      // left
-      if (results[0].probability.toFixed(2) > 0.9) {
-        if (timeSeconds >= 0 && timeSeconds <= 2) {
-          if (!checkCount0) {
-            console.log(results[0].className + " 인식 => 경과 시간 : " + timeSeconds + "초");
-            checkCount0 = true; // 반복문 안에서 setState 쓰면 리렌더링이 안되므로 쓰면 X
-            setCorrectCount(++cnt);
+        // side_up
+        if (results[0].probability.toFixed(2) > 0.9) {
+          if (
+            (t >= 10 && t <= 12) ||
+            (t >= 17 && t <= 19) ||
+            (t >= 22 && t <= 24) ||
+            (t >= 27 && t <= 29) ||
+            (t >= 32 && t <= 34) ||
+            (t >= 42 && t <= 44)
+          ) {
+            if (!m0c0) {
+              console.log(results[0].className + " 인식 => 경과 시간 : " + t + "초");
+              m0c0 = true; // 반복문 안에서 setState 쓰면 리렌더링이 안되므로 쓰면 X
+              setCorrectCount(++cnt);
+            }
           }
         }
-      }
-      // right
-      else if (results[1].probability.toFixed(2) > 0.9) {
-        if (timeSeconds >= 3 && timeSeconds <= 4) {
-          if (!checkCount1) {
-            console.log(results[1].className + " 인식 => 경과 시간 : " + timeSeconds + "초");
-            checkCount1 = true;
-            setCorrectCount(++cnt);
+        // side_down
+        else if (results[1].probability.toFixed(2) > 0.9) {
+          if (
+            (t >= 13 && t <= 15) ||
+            (t >= 20 && t <= 22) ||
+            (t >= 24 && t <= 26) ||
+            (t >= 29 && t <= 31) ||
+            (t >= 43 && t <= 45)
+          ) {
+            if (!m0c1) {
+              console.log(results[1].className + " 인식 => 경과 시간 : " + t + "초");
+              m0c1 = true;
+              setCorrectCount(++cnt);
+            }
+          }
+        }
+        // basic_up
+        else if (results[2].probability.toFixed(2) > 0.9) {
+          if ((t >= 37 && t <= 39) || (t >= 42 && t <= 44)) {
+            if (!m0c2) {
+              console.log(results[2].className + " 인식 => 경과 시간 : " + t + "초");
+              m0c2 = true;
+              setCorrectCount(++cnt);
+            }
+          }
+        }
+        // basic_down
+        else if (results[3].probability.toFixed(2) > 0.9) {
+          if (
+            (t >= 35 && t <= 37) ||
+            (t >= 44 && t <= 46) ||
+            (t >= 56 && t <= 58) ||
+            (t >= 59 && t <= 61)
+          ) {
+            if (!m0c3) {
+              console.log(results[3].className + " 인식 => 경과 시간 : " + t + "초");
+              m0c3 = true;
+              setCorrectCount(++cnt);
+            }
+          }
+        }
+        // basic_up2
+        else if (results[4].probability.toFixed(2) > 0.9) {
+          if ((t >= 39 && t <= 41) || (t >= 46 && t <= 48)) {
+            if (!m0c4) {
+              console.log(results[4].className + " 인식 => 경과 시간 : " + t + "초");
+              m0c4 = true;
+              setCorrectCount(++cnt);
+            }
+          }
+        }
+        // basic_down2
+        else if (results[5].probability.toFixed(2) > 0.9) {
+          if ((t >= 38 && t <= 40) || (t >= 45 && t <= 47)) {
+            if (!m0c5) {
+              console.log(results[5].className + " 인식 => 경과 시간 : " + t + "초");
+              m0c5 = true;
+              setCorrectCount(++cnt);
+            }
+          }
+        }
+        // down3_left
+        else if (results[6].probability.toFixed(2) > 0.9) {
+          if ((t >= 50 && t <= 52) || (t >= 53 && t <= 55)) {
+            if (!m0c6) {
+              console.log(results[6].className + " 인식 => 경과 시간 : " + t + "초");
+              m0c6 = true;
+              setCorrectCount(++cnt);
+            }
+          }
+        }
+        // down3_right
+        else if (results[7].probability.toFixed(2) > 0.9) {
+          if ((t >= 48 && t <= 50) || (t >= 51 && t <= 53)) {
+            if (!m0c7) {
+              console.log(results[7].className + " 인식 => 경과 시간 : " + t + "초");
+              m0c7 = true;
+              setCorrectCount(++cnt);
+            }
+          }
+        }
+        // down4_left
+        else if (results[8].probability.toFixed(2) > 0.9) {
+          if ((t >= 62 && t <= 64) || (t >= 66 && t <= 68)) {
+            if (!m0c8) {
+              console.log(results[8].className + " 인식 => 경과 시간 : " + t + "초");
+              m0c8 = true;
+              setCorrectCount(++cnt);
+            }
+          }
+        }
+        // down4_left
+        else if (results[9].probability.toFixed(2) > 0.9) {
+          if ((t >= 61 && t <= 63) || (t >= 64 && t <= 66)) {
+            if (!m0c9) {
+              console.log(results[9].className + " 인식 => 경과 시간 : " + t + "초");
+              m0c9 = true;
+              setCorrectCount(++cnt);
+            }
           }
         }
       }
