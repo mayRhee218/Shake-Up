06c0743a0e7019327d0d38a7b12ef786b35f03e9
ekdms4213 <38871826+ekdms4213@users.noreply.github.com>
2022-02-14 16:12:56 +0900

    update develop

--- FE/src/components/views/dandda/danddamain/basic/DanddaContent.js
+++ FE/src/components/views/dandda/danddamain/basic/DanddaContent.js
@@ -21,82 +21,80 @@ function Arrow(props) {
   return <div onClick={clickFunction}>{icon}</div>;
 }
 
-function DanddaContent () {
-    useEffect(() => {
-        const handleKeyDown = (e) => {
-            if (e.keyCode === 39) {
-                onArrowClick('right');
-            }
-            if (e.keyCode === 37) {
-                onArrowClick('left');
-            }
-        }
-    
-        window.addEventListener('keydown', handleKeyDown);
-
-        return () => {
-            window.removeEventListener('keydown', handleKeyDown);
-        };
-    }, []);
-    
-
-const SLIDE_INFO = [
-        {
-            backgroundImage: `url(${img1})`,
-            title: '바운스',
-            profile_name: "seoyoung",
-            profile_src: profile_src,
-            id: 4,
-            uid:2,
-            url: 'https://firebasestorage.googleapis.com/v0/b/dance-704a8.appspot.com/o/videos%2F12345.mp4?alt=media&token=b2d33474-a957-49b1-946c-a699cc9f6209',
-            turl: '',
-          },
-        {
-            backgroundImage: `url(${img2})`,
-            title: '피치스',
-            profile_name: "seoyoung",
-            profile_src: profile_src,
-            id: 2,
-            uid:2,
-            url: 'https://firebasestorage.googleapis.com/v0/b/dance-704a8.appspot.com/o/videos%2F1644628771425?alt=media&token=11872f38-890f-4c3e-8ba0-b938aed11a42',
-            turl: ''
-          },
-        {
-            backgroundImage: `url(${img3})`,
-            title: '아이솔레이션',
-            profile_name: "seoyoung",
-            profile_src: profile_src,
-            id: 3,
-            uid:2,
-            url: 'https://firebasestorage.googleapis.com/v0/b/dance-704a8.appspot.com/o/videos%2F1644500263394?alt=media&token=b922bd00-e620-40ff-ad79-7817e0d19a12',
-            turl: ''
-          },
-    ]
-
-    const [index, setIndex] = useState(0);
-    const content = SLIDE_INFO[index];
-    const numSlides = SLIDE_INFO.length;
-
-    const [slideIn, setSlideIn] = useState(true);
-    const [slideDirection, setSlideDirection] = useState('down');
-
-    const navigate = useNavigate();
-
-    const onArrowClick = (direction) => {
-        const increment = direction === 'left' ? -1 : 1;
-        const newIndex = (index + increment + numSlides) % numSlides;
-
-        const oppDirection = direction === 'left' ? 'right' : 'left';
-        setSlideDirection(direction);
-        setSlideIn(false);
-
-        setTimeout(() => {
-            setIndex(newIndex);
-            setSlideDirection(oppDirection);
-            setSlideIn(true);
-        }, 500);
+function DanddaContent() {
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.keyCode === 39) {
+        onArrowClick("right");
+      }
+      if (e.keyCode === 37) {
+        onArrowClick("left");
+      }
     };
 
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
+  const SLIDE_INFO = [
+    {
+      backgroundImage: `url(${img1})`,
+      title: "바운스",
+      profile_name: "seoyoung",
+      profile_src: profile_src,
+      id: 4,
+      uid: 2,
+      url: "https://firebasestorage.googleapis.com/v0/b/dance-704a8.appspot.com/o/videos%2F12345.mp4?alt=media&token=b2d33474-a957-49b1-946c-a699cc9f6209",
+      turl: "https://teachablemachine.withgoogle.com/models/NuAS299xH/",
+    },
+    {
+      backgroundImage: `url(${img2})`,
+      title: "피치스",
+      profile_name: "seoyoung",
+      profile_src: profile_src,
+      id: 2,
+      uid: 2,
+      url: "https://firebasestorage.googleapis.com/v0/b/dance-704a8.appspot.com/o/videos%2F1644628771425?alt=media&token=11872f38-890f-4c3e-8ba0-b938aed11a42",
+      turl: "https://teachablemachine.withgoogle.com/models/h3cg54y28/",
+    },
+    {
+      backgroundImage: `url(${img3})`,
+      title: "아이솔레이션",
+      profile_name: "seoyoung",
+      profile_src: profile_src,
+      id: 3,
+      uid: 2,
+      url: "https://firebasestorage.googleapis.com/v0/b/dance-704a8.appspot.com/o/videos%2F1644500263394?alt=media&token=b922bd00-e620-40ff-ad79-7817e0d19a12",
+      turl: "",
+    },
+  ];
+
+  const [index, setIndex] = useState(0);
+  const content = SLIDE_INFO[index];
+  const numSlides = SLIDE_INFO.length;
+
+  const [slideIn, setSlideIn] = useState(true);
+  const [slideDirection, setSlideDirection] = useState("down");
+
+  const navigate = useNavigate();
+
+  const onArrowClick = (direction) => {
+    const increment = direction === "left" ? -1 : 1;
+    const newIndex = (index + increment + numSlides) % numSlides;
+
+    const oppDirection = direction === "left" ? "right" : "left";
+    setSlideDirection(direction);
+    setSlideIn(false);
+
+    setTimeout(() => {
+      setIndex(newIndex);
+      setSlideDirection(oppDirection);
+      setSlideIn(true);
+    }, 500);
+  };
 
   return (
     <div className="DanddaMain">
