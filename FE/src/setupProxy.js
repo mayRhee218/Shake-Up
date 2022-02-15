fe88af4e487f82026796684eb862c51712e95d26
SSAFY <2017113427@dgu.ac.kr>
2022-02-15 09:49:50 +0900

    proxy 수정

--- FE/src/setupProxy.js
+++ FE/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://i6d103.p.ssafy.io/',
+      target: 'http://i6d103.p.ssafy.io:8181/',
       changeOrigin: true,
     })
   );
