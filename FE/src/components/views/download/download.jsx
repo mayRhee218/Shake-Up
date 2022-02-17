5f6b86b19843f3954b3059e08e7a94dfa81ffa25
junyeong <qres3408@gmail.com>
2022-02-18 04:46:12 +0900

    update feature/FE/download

--- FE/src/components/views/download/download.jsx
+++ FE/src/components/views/download/download.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import './download.css'
 
 function download() {
+  const onclick = () => {
+    alert('엥이')
+  }
   return (
     <div className='body'>
+      <div className='bar'></div>
+      <span className="down">click 👇</span>
       <div className="wrapper">
-        <h1 className='h1' data-heading="Shake Up" contenteditable>Shake Up</h1>
+        <h1 className='h1' data-heading="Shake Up" onClick={onclick}>Shake Up</h1>
       </div>
     </div>
   );
